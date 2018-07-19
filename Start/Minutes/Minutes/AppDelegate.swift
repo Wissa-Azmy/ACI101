2c8645deae7b735e8fbd6c49e770c0703a12ed82
Wissa Azmy <wissaaymz@wissas-macbook-pro.local>
2018-07-19 14:24:34 +0200

    Installed AppCenter SDK, Initialized AppCenter in the AppDelegate

--- Start/Minutes/Minutes/AppDelegate.swift
+++ Start/Minutes/Minutes/AppDelegate.swift
@@ -1,13 +1,21 @@
 import UIKit
+import AppCenter
+import AppCenterDistribute
+import AppCenterCrashes
+import AppCenterAnalytics
+import AppCenterPush
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate
 {
     static var entries: FileEntryStore = FileEntryStore()
+    private var appCenterSecretKey = "7123e48a-31fa-4eb7-ab5f-a30ebe0e921d"
     
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        MSAppCenter.start(appCenterSecretKey, withServices: [MSDistribute.self, MSCrashes.self, MSAnalytics.self, MSPush.self])
 
         return true
     }
